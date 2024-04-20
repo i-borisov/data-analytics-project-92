ad2e8e9c1e387daf964bb9fac529eda0e5e73339
i-borisov <165560297+i-borisov@users.noreply.github.com>
2024-04-20 14:20:48 +0300

    v6

--- queries.sql
+++ queries.sql
@@ -40,7 +40,7 @@ order by average_income asc;
 with tab as (
     select
         CONCAT(e.first_name, ' ', e.last_name) as seller,
-        to_char(s.sale_date, 'day') as day_of_week,
+        TO_CHAR(s.sale_date, 'day') as day_of_week,
         EXTRACT(isodow from s.sale_date) as day_number,
         FLOOR(SUM(s.quantity * p.price)) as income
     from sales as s
@@ -58,43 +58,42 @@ from tab;
 
 --количество покупателей по возрастным группам:
 select
-case
-when c.age between 16 and 25 then '16-25'
-when c.age between 26 and 40 then '26-40'
-when age > 40 then '40+'
-end as age_category,
-COUNT(c.age) as age_count
-from customers c
+    case
+        when c.age between 16 and 25 then '16-25'
+        when c.age between 26 and 40 then '26-40'
+        when c.age > 40 then '40+'
+    end as age_category,
+    COUNT(c.age) as age_count
+from customers as c
 group by age_category
 order by age_category;
 
 --количество покупателей и выручка по месяцам:
 select
-to_char(sale_date, 'YYYY-MM') as selling_month,
-COUNT(distinct customer_id) as total_customers,
-FLOOR(SUM(s.quantity * p.price)) as income
-from sales s
-left join products p on s.product_id = p.product_id 
+    TO_CHAR(sale_date, 'YYYY-MM') as selling_month,
+    COUNT(distinct customer_id) as total_customers,
+    FLOOR(SUM(s.quantity * p.price)) as income
+from sales as s
+left join products as p on s.product_id = p.product_id
 group by selling_month
-order by selling_month ASC;
+order by selling_month asc;
 
---покупатели, первая покупка которых пришлась на время проведения специальных акций:
+--покупатели, первая покупка которых пришлась на время специальных акций:
 with tab as (
-select 
-distinct s.customer_id,
+    select distinct s.customer_id,
 p.price,
 first_value(s.sales_id) OVER(partition by s.customer_id order by s.sale_date asc, p.price asc) as sales_id
-from sales s  
+from sales as s
 left join products p on s.product_id = p.product_id
 where price = 0
 )
 
 select
-CONCAT(c.first_name, ' ', c.last_name) as customer,
-s.sale_date,
-CONCAT(e.first_name, ' ', e.last_name) as seller
-from sales s 
-left join customers c ON s.customer_id = c.customer_id 
-left join employees e on s.sales_person_id = e.employee_id
+    CONCAT(c.first_name, ' ', c.last_name) as customer,
+    s.sale_date,
+    CONCAT(e.first_name, ' ', e.last_name) as seller
+from sales as s
+left join customers as c ON s.customer_id = c.customer_id
+left join employees as e on s.sales_person_id = e.employee_id
 where s.sales_id in (select tab.sales_id from tab)
 order by s.customer_id;
